929a16a4ad7565b00cf17b1916ac6901d078dd04
ceylino <ceylin_ozdemir@hotmail.com>
2017-07-15 16:43:33 -0700

    Ceylin

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,19 +1,42 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import "Question_data_sample.js"
+//import "Question_data_sample.js"
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h1>Welcome to Feel Better</h1>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <div className="App-description">
+          <div className="Header1">
+            <h2>We are here to lift your mood in less than 5 minutes!</h2>
+          </div>
+          <div className="Header2">
+            <h3>By answering some questions for us, we will help you find an activity that you may enjoy doing.</h3>
+          </div>
+          <div className="Header">
+            <h3>Let's get to it</h3>
+          </div>
+        </div>
+
+        <div className="Box">
+          <div className="Question">
+            Are you having a good day?
+          </div>
+          <div className="Button">
+            Yes
+            No
+          </div>
+
+          
+
+        </div>
+
+
       </div>
     );
   }
