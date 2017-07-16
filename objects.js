197f11d67fbd9caafab68f13c0cc8e9b7a3fa48f
Lironne <lironkurz@gmail.com>
2017-07-15 20:09:38 -0700

    Add existing file

---
+++ objects.js
@@ -0,0 +1,11 @@
+var Questions = 
+[ {id: 0, question: "Are you having a good day?", nextT: 2, nextF: 3}
+  {id: 1, question: "Is it better than yesterday?", nextT: 2, nextF: 3}
+  {id: 2, question: "Any progress is improvement! are you having negative thoughts?", nextT: 7, nextF: 8}
+  {id: 3, question: "Lets take it one day at a time! can you think of a positive memory?", nextT: 0, nextF: 0}
+  {id: 4, question: "Are you having trouble concentrating?", nextT: 5, nextF: 6}
+  {id: 5, question: "Are you having trouble breathing?", nextT: 0, nextF: 0}
+  {id: 6, question: "Are you having negative thoughts?", nextT: 7, nextF: 8}
+  {id: 7, question: "Are you thiking of harming yourself in any way?", nextT: 0, nextF: 0}
+  {id: 8, question: "Did you eat in the last 3 hours?", nextT: 0, nextF: 0}
+];
